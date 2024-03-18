db184a1ef01d3ae9a1f1c05adef3b5e360ccf490
ChriCore99 <christian.corelli.io@gmail.com>
2024-03-18 14:53:20 +0100

    ADD: aggiunti membri e info come oggetti

---
+++ js/app.js
@@ -0,0 +1,38 @@
+// CREA L'ARRAY DI OGGETTI PER RAPPRESENTARE I MEMBRI DEL TEAM
+const membriTeam = [
+    {
+        nome: 'Wayne Barnett', 
+        lavoro: 'Founder & CEO',
+        foto: 'Wayne-barnett-founder-ceo.jpg',
+    },
+
+    {
+        nome: 'Angela Caroll', 
+        lavoro: 'Chief Editor',
+        foto: 'angela-caroll-chief-editor.jpg',
+    },
+
+    {
+        nome: 'Walter Gordon', 
+        lavoro: 'Office Manager',
+        foto: 'walter-gordon-office-manager.jpg',
+    },
+
+    {
+        nome: 'Angela Lopez', 
+        lavoro: 'Social Media Manager',
+        foto: 'angela-lopez-social-media-manager.jpg',
+    },
+
+    {
+        nome: 'Scott Estrada', 
+        lavoro: 'Developer',
+        foto: 'scott-estrada-developer.jpg',
+    },
+
+    {
+        nome: 'Barbara Ramos', 
+        lavoro: 'Graphic Designer',
+        foto: 'barbara-ramos-graphic-designer.jpg',
+    }
+];
